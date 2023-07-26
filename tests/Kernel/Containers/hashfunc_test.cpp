de4aa250dac142ffd32b53a1e3146e64277f8034
charonxin <1656746727@qq.com>
2023-07-26 20:56:52 +0800

    fix

--- tests/Kernel/Containers/hashfunc_test.cpp
+++ tests/Kernel/Containers/hashfunc_test.cpp
@@ -4,7 +4,7 @@
 #include "hashfunc.hpp"
 
 using namespace lolly;
-using lolly:tree_label;
+using lolly::tree_label;
 
 TEST_CASE ("hashfunc") {
   // Define the hash function for integers, where hash(x) = x % 7
