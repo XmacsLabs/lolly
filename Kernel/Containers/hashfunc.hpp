df0bce82c3ebdd9e9cf012299ae2b5f74afaff60
CharonXin <91298205+Charonxin@users.noreply.github.com>
2023-07-26 13:31:32 +0800

    [1_1] doc for hashfunc.hpp (#7)
    * [1_1] doc for hashfunc.hpp

--- Kernel/Containers/hashfunc.hpp
+++ Kernel/Containers/hashfunc.hpp
@@ -1,32 +1,68 @@
-
-/******************************************************************************
- * MODULE     : hashfunc.hpp
- * DESCRIPTION: functions with a memorizing hash table
- * COPYRIGHT  : (C) 1999  Joris van der Hoeven
- *******************************************************************************
- * This software falls under the GNU general public license version 3 or later.
- * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
- * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
- ******************************************************************************/
+/** \file hashfunc.hpp
+ *  \copyright GPLv3
+ *  \details This hashfunc is a generic hash function class that utilizes a
+ *           concrete implementation of a hash function for a given type.
+ *  \author Joris van der Hoeven
+ *  \date   1999
+ */
 
 #ifndef HASHFUNC_H
 #define HASHFUNC_H
 #include "hashmap.hpp"
 
+/**
+ * @brief A concrete implementation of a hash function for a given type.
+ * @tparam T The input type of the hash function.
+ * @tparam U The output type of the hash function.
+ */
 template <class T, class U> class hashfunc_rep : public concrete_struct {
-  U (*func) (T);          // the function
-  hashmap<T, U> remember; // remembered values
+  U (*func) (T);          /**< A pointer to the hash function. */
+  hashmap<T, U> remember; /**< A hash map to store remembered values. */
 public:
+  /**
+   * @brief Constructor for hashfunc_rep.
+   * @param func2 A pointer to the hash function to be used.
+   * @param init The initial value for the hash map.
+   */
   inline hashfunc_rep (U (*func2) (T), U init)
       : func (func2), remember (init) {}
+
+  /**
+   * @brief Applies the hash function to the given input.
+   * @param x The input value to be hashed.
+   * @return The output of the hash function for the given input.
+   */
   U apply (T x);
 };
 
+/**
+ * @brief A generic hash function class that uses a concrete implementation of a
+ * hash function.
+ * @tparam T The input type of the hash function.
+ * @tparam U The output type of the hash function.
+ */
 template <class T, class U> class hashfunc {
   CONCRETE_TEMPLATE_2 (hashfunc, T, U);
+
+  /**
+   * @brief Constructor for hashfunc.
+   * @param func A pointer to the hash function to be used.
+   * @param init The initial value for the hash map.
+   */
   inline hashfunc (U (*func) (T), U init)
       : rep (tm_new<hashfunc_rep<T, U>> (func, init)) {}
+
+  /**
+   * @brief Applies the hash function to the given input.
+   * @param x The input value to be hashed.
+   * @return The output of the hash function for the given input.
+   */
   inline U operator[] (T x) { return rep->apply (x); }
+
+  /**
+   * @brief Conversion operator to convert the hash function object to a tree.
+   * @return A tree representing the hash function object.
+   */
   operator tree ();
 };
 CONCRETE_TEMPLATE_2_CODE (hashfunc, class, T, class, U);
