f881a7df897c8da939fa75a568b5b7bad635a0c0
charonxin <1656746727@qq.com>
2023-07-27 21:27:52 +0800

    update

--- tests/Kernel/Containers/hashtree_test.cpp
+++ tests/Kernel/Containers/hashtree_test.cpp
@@ -7,35 +7,54 @@
 #include "string.hpp"
 
 // Create a hashtree object with integer keys and string values
-hashtree_rep<int, string> t;
-
-string a ("one");
-string b ("two");
-
-hashtree<int, string> child1 (string ("one"));
-hashtree<int, string> child2 (string ("two"));
 
 TEST_CASE ("Test the add_child function") {
-  t.add_child (1, child1);
-  t.add_child (2, child2);
+  hashtree<int, string> t;
+
+  string a ("one");
+  string b ("two");
+
+  hashtree<int, string> child1 (string ("one"));
+  hashtree<int, string> child2 (string ("two"));
+  t->add_child (1, child1);
+  t->add_child (2, child2);
   CHECK (N (child1) == 0);
 }
 
 TEST_CASE ("Test the contains function") {
-  CHECK (t.contains (1));
-  CHECK (t.contains (2));
-  CHECK (!t.contains (3));
+  hashtree<int, string> t;
+
+  string a ("one");
+  string b ("two");
+
+  hashtree<int, string> child1 (string ("one"));
+  hashtree<int, string> child2 (string ("two"));
+  t->add_child (1, child1);
+  t->add_child (2, child2);
+  CHECK (t->contains (1));
+  CHECK (t->contains (2));
+  CHECK (!t->contains (3));
 }
 
 TEST_CASE ("Test the add_new_child function") {
-  t.add_new_child (3);
+  hashtree<int, string> t;
+
+  string a ("one");
+  string b ("two");
+
+  hashtree<int, string> child1 (string ("one"));
+  hashtree<int, string> child2 (string ("two"));
+  t->add_child (1, child1);
+  t->add_child (2, child2);
+  t->add_new_child (3);
   CHECK_EQ (N (child2) == 3, false);
-  CHECK (t.contains (3));
+  CHECK (t->contains (3));
 }
 
 TEST_CASE ("Test the set_label and get_label function") {
-  t.set_label ("root");
-  CHECK (t.get_label () == "root");
+  hashtree<int, string> t;
+  t->set_label ("root");
+  CHECK (t->get_label () == "root");
 }
 
 TEST_CASE ("Test the N and is_nil functions") {
