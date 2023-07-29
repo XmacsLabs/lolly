9044ab45af5c57307fb5986c41183c12963b0bf0
沈浪熊猫儿 <shenda@ustc.edu>
2023-07-29 15:18:53 +0800

    Improve initptr

--- Kernel/Abstractions/basic.cpp
+++ Kernel/Abstractions/basic.cpp
@@ -9,17 +9,6 @@
  * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
  ******************************************************************************/
 
-#ifdef HAVE_INTPTR_T
-#ifdef HAVE_INTTYPES_H
-#include <inttypes.h>
-#endif
-#ifdef HAVE_STDINT_H
-#include <stdint.h>
-#endif
-#else
-typedef long intptr_t;
-#endif
-
 #include "analyze.hpp"
 #include "string.hpp"
 
