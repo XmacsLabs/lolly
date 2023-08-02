1915b37220bd5597299b9785881b0c9ca3e99998
Paradisum <2513209473@qq.com>
2023-08-02 23:58:47 +0800

    add clang-format

--- tests/Kernel/Containers/rel_hashmap_test.cpp
+++ tests/Kernel/Containers/rel_hashmap_test.cpp
@@ -2,28 +2,25 @@
 
 #include "doctest/doctest.h"
 #include "rel_hashmap.hpp"
-#include "string.hpp" 
 
 TEST_CASE ("test contains") {
-  rel_hashmap<int, int> t(0);
+  rel_hashmap<int, int> t (0);
 
-  SUBCASE ("test case is empty") {
-    CHECK (t->contains (1) == false);
-  }
+  SUBCASE ("test case is empty") { CHECK (t->contains (1) == false); }
 
   SUBCASE ("test case is not empty") {
-    hashmap<int, int> hm1(0, 10);
-    hm1 (1)=          10;
-    hm1 (2)=          20;
+    hashmap<int, int> hm1 (0, 10);
+    hm1 (1)= 10;
+    hm1 (2)= 20;
     t->change (hm1);
 
     CHECK (t->contains (1) == true);
     CHECK (t->contains (2) == true);
     CHECK (t->contains (3) == false);
 
-    hashmap<int, int> hm2(0, 10);
-    hm2 (3)=          30;
-    hm2 (4)=          40;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (3)= 30;
+    hm2 (4)= 40;
     t->extend ();
     t->change (hm2);
 
@@ -35,11 +32,11 @@ TEST_CASE ("test contains") {
 }
 
 TEST_CASE ("test extend") {
-  rel_hashmap<int, int> t(0);
+  rel_hashmap<int, int> t (0);
 
-  hashmap<int, int>     hm1(0, 10);
-  hm1 (1)=              10;
-  hm1 (2)=              20;
+  hashmap<int, int> hm1 (0, 10);
+  hm1 (1)= 10;
+  hm1 (2)= 20;
   t->change (hm1);
   t->extend ();
 
@@ -49,7 +46,7 @@ TEST_CASE ("test extend") {
 }
 
 TEST_CASE ("test shorten") {
-  rel_hashmap<int, int> t(0);
+  rel_hashmap<int, int> t (0);
 
   SUBCASE ("test Shorten the empty rel_hashmap") {
     CHECK_THROWS_AS (t->shorten (), string);
@@ -63,32 +60,31 @@ TEST_CASE ("test shorten") {
     }
 
     SUBCASE ("test the content") {
-      t->extend();
-      hashmap<int, int> hm1(0, 10);
-      hm1 (1)=          10;
-      hm1 (2)=          20;
+      t->extend ();
+      hashmap<int, int> hm1 (0, 10);
+      hm1 (1)= 10;
+      hm1 (2)= 20;
       t->change (hm1);
       t->shorten ();
-    
+
       CHECK (t->contains (1) == false);
       CHECK (t->contains (2) == false);
       CHECK (t->contains (4) == false);
       CHECK (is_nil (t->next));
     }
-
   }
 }
 
 TEST_CASE ("test merge") {
-  rel_hashmap<int, int> t(0);
+  rel_hashmap<int, int> t (0);
 
   SUBCASE ("test merge the empty rel_hashmap") {
     CHECK_THROWS_AS (t->merge (), string);
   }
 
-  hashmap<int, int>     hm1(0, 10);
-  hm1 (1)=              10;
-  hm1 (2)=              20;
+  hashmap<int, int> hm1 (0, 10);
+  hm1 (1)= 10;
+  hm1 (2)= 20;
   t->change (hm1);
   t->extend ();
 
@@ -101,33 +97,32 @@ TEST_CASE ("test merge") {
 
     SUBCASE ("test the content") {
       t->extend ();
-      hashmap<int, int>     hm2(0, 10);
-      hm2 (3)=              30;
-      hm2 (4)=              40;
+      hashmap<int, int> hm2 (0, 10);
+      hm2 (3)= 30;
+      hm2 (4)= 40;
       t->change (hm2);
       t->merge ();
-    
+
       CHECK (t->contains (1) == true);
       CHECK (t->contains (2) == true);
       CHECK (t->contains (3) == true);
       CHECK (t->contains (4) == true);
       CHECK (t->contains (5) == false);
     }
-
   }
 }
 
 TEST_CASE ("test find_changes") {
-  rel_hashmap<int, int> t(0);
-  hashmap<int, int>     hm1(0, 10);
-  hm1 (1)=              10;
-  hm1 (2)=              20;
+  rel_hashmap<int, int> t (0);
+  hashmap<int, int>     hm1 (0, 10);
+  hm1 (1)= 10;
+  hm1 (2)= 20;
   t->change (hm1);
 
   SUBCASE ("test if all the same") {
-    hashmap<int, int>     hm2(0, 10);
-    hm2 (1)=             10;
-    hm2 (2)=             20;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (1)= 10;
+    hm2 (2)= 20;
     t->find_changes (hm2);
 
     CHECK (hm2->contains (1) == false);
@@ -135,9 +130,9 @@ TEST_CASE ("test find_changes") {
   }
 
   SUBCASE ("test if not same") {
-    hashmap<int, int>     hm2(0, 10);
-    hm2 (1)=              10;
-    hm2 (2)=              30;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (1)= 10;
+    hm2 (2)= 30;
     t->find_changes (hm2);
 
     CHECK (hm2->contains (1) == false);
@@ -145,9 +140,9 @@ TEST_CASE ("test find_changes") {
   }
 
   SUBCASE ("test if all different") {
-    hashmap<int, int>     hm2(0, 10);
-    hm2 (1)=              20;
-    hm2 (2)=              30;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (1)= 20;
+    hm2 (2)= 30;
     t->find_changes (hm2);
 
     CHECK (hm2[1] == 20);
@@ -156,34 +151,34 @@ TEST_CASE ("test find_changes") {
 }
 
 TEST_CASE ("test find_differences") {
-  rel_hashmap<int, int> t(0);
-  hashmap<int, int>     hm1(0, 10);
-  hm1 (1)=              10;
-  hm1 (2)=              20;
+  rel_hashmap<int, int> t (0);
+  hashmap<int, int>     hm1 (0, 10);
+  hm1 (1)= 10;
+  hm1 (2)= 20;
   t->change (hm1);
 
   SUBCASE ("test if all the same") {
-    hashmap<int, int> hm2(0, 10);
-    hm2 (1)=          10;
-    hm2 (2)=          20;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (1)= 10;
+    hm2 (2)= 20;
     t->find_differences (hm2);
 
     CHECK (hm2->contains (1) == false);
     CHECK (hm2->contains (2) == false);
   }
 
-  t->extend();
-  hashmap<int, int>     hm2(0, 10);
-  hm1 (3)=              30;
-  hm1 (4)=              40;
+  t->extend ();
+  hashmap<int, int> hm2 (0, 10);
+  hm1 (3)= 30;
+  hm1 (4)= 40;
   t->change (hm2);
 
   SUBCASE ("test if key different") {
-    
+
     SUBCASE ("test if different from first, but the same as next") {
       hashmap<int, int> hm3 (0, 10);
-      hm3 (3)=          30;
-      hm3 (4)=          40;
+      hm3 (3)= 30;
+      hm3 (4)= 40;
       t->find_differences (hm3);
 
       CHECK (hm3->contains (1) == false);
@@ -194,8 +189,8 @@ TEST_CASE ("test find_differences") {
 
     SUBCASE ("test if different from all the others") {
       hashmap<int, int> hm3 (0, 10);
-      hm3 (5)=          30;
-      hm3 (6)=          40;
+      hm3 (5)= 30;
+      hm3 (6)= 40;
       t->find_differences (hm3);
 
       CHECK (hm3->contains (1) == false);
@@ -204,8 +199,8 @@ TEST_CASE ("test find_differences") {
       CHECK (hm3[6] == 40);
 
       hashmap<int, int> hm4 (0, 10);
-      hm4 (3)=          40;
-      hm4 (4)=          50;
+      hm4 (3)= 40;
+      hm4 (4)= 50;
       t->find_differences (hm4);
 
       CHECK (hm4->contains (1) == false);
@@ -213,32 +208,31 @@ TEST_CASE ("test find_differences") {
       CHECK (hm4[3] == 40);
       CHECK (hm4[4] == 50);
     }
-
   }
 }
 
 TEST_CASE ("test change") {
-  rel_hashmap<int, int> t(0);
-  hashmap<int, int>     hm1(0, 10);
-  hm1 (1)=              10;
-  hm1 (2)=              20;
+  rel_hashmap<int, int> t (0);
+  hashmap<int, int>     hm1 (0, 10);
+  hm1 (1)= 10;
+  hm1 (2)= 20;
   t->change (hm1);
 
   CHECK (t->contains (1));
   CHECK (t->contains (2));
 
   SUBCASE ("test change the same") {
-    hashmap<int, int> hm2(0, 10);
-    hm2 (1)=          10;
-    hm2 (2)=          20;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (1)= 10;
+    hm2 (2)= 20;
     t->change (hm2);
 
     CHECK (t->contains (1));
     CHECK (t->contains (2));
 
-    hashmap<int, int> hm3(0, 10);
-    hm3 (1)=          20;
-    hm3 (2)=          30;
+    hashmap<int, int> hm3 (0, 10);
+    hm3 (1)= 20;
+    hm3 (2)= 30;
     t->change (hm3);
 
     CHECK (t[1] == 20);
@@ -246,9 +240,9 @@ TEST_CASE ("test change") {
   }
 
   SUBCASE ("test change the different") {
-    hashmap<int, int> hm2(0, 10);
-    hm2 (3)=          30;
-    hm2 (4)=          40;
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (3)= 30;
+    hm2 (4)= 40;
     t->change (hm2);
 
     CHECK (t[1] == 10);
@@ -258,10 +252,10 @@ TEST_CASE ("test change") {
   }
 
   SUBCASE ("test change the next") {
-    t->extend();
-    hashmap<int, int> hm2(0, 10);
-    hm2 (3)=          30;
-    hm2 (4)=          40;
+    t->extend ();
+    hashmap<int, int> hm2 (0, 10);
+    hm2 (3)= 30;
+    hm2 (4)= 40;
     t->change (hm2);
 
     CHECK (t[1] == 10);
