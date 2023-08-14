a30876fd948e2260c86d0df0e5bdd0c8a79daf40
charonxin <1656746727@qq.com>
2023-08-14 10:33:37 +0800

    [2_3] migrete tree_test from mogan

---
+++ tests/Kernel/Types/tree_test.cpp
@@ -0,0 +1,65 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+#include "tree.hpp"
+
+TEST_CASE ("test_is_atomic") { CHECK (is_atomic (tree ())); }
+
+TEST_CASE ("test_is_compound") {
+  CHECK (is_compound (tree (/*FRAC*/ 70, 3)));
+  CHECK (!is_compound (tree ()));
+}
+
+TEST_CASE ("test_is_bool") {
+  CHECK (is_bool (tree ("true")));
+  CHECK (is_bool (tree ("false")));
+  CHECK (!is_bool (tree ("other")));
+}
+
+TEST_CASE ("test_is_int") {
+  CHECK (!is_int (tree ()));
+  CHECK (is_int (tree ("+12")));
+  CHECK (is_int (tree ("-12")));
+  CHECK (!is_int (tree ("-+12")));
+  CHECK (!is_int (tree ("one")));
+}
+
+TEST_CASE ("test_is_func") {
+  CHECK (!is_func (tree (70, 3), 5));
+  CHECK (!is_func (tree (), 0));
+  CHECK (is_func (tree (67, 4), 67));
+}
+
+TEST_CASE ("test_is_double") {
+  CHECK (!is_double (tree ()));
+  CHECK (is_double (tree ("3.15")));
+  CHECK (is_double (tree ("0")));
+  CHECK (is_double (tree ("0.0")));
+  CHECK (is_double (tree ("3.1415926")));
+  CHECK (is_double (tree ("-3.1415926")));
+  CHECK (!is_double (tree ("nothing")));
+}
+
+TEST_CASE ("test_is_string") {
+  CHECK (is_string (tree ()));
+  CHECK (is_string (tree ("0")));
+  CHECK (is_string (tree ("+121234")));
+  CHECK (is_string (tree ("seigj")));
+  CHECK (is_string (tree ("!@#$$!")));
+  CHECK (!is_string (tree (70, 3)));
+}
+
+TEST_CASE ("test_is_generic") {
+  CHECK (!is_generic (tree (9, 0)));
+  CHECK (!is_generic (tree (10, -1)));
+  CHECK (!is_generic (tree (11, 1234)));
+  CHECK (!is_generic (tree (11, 4)));
+  CHECK (!is_generic (tree (12, 2)));
+}
+
+TEST_CASE ("test_arity") {
+  CHECK (!arity (tree (0, -1)));
+  CHECK (!arity (tree (280, 0)));
+  CHECK (arity (tree (9, 3)));
+  CHECK (!arity (tree (0, 5)));
+}
