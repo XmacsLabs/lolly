cd21ef0cef416c055081176a86bd05e0ab31708e
charonxin <1656746727@qq.com>
2023-08-14 10:40:43 +0800

    add more assert for tree_test

--- tests/Kernel/Types/tree_test.cpp
+++ tests/Kernel/Types/tree_test.cpp
@@ -3,11 +3,18 @@
 #include "doctest/doctest.h"
 #include "tree.hpp"
 
-TEST_CASE ("test_is_atomic") { CHECK (is_atomic (tree ())); }
+TEST_CASE ("test_is_atomic") {
+  CHECK (is_atomic (tree ()));
+  CHECK (is_atomic (tree (0, tree ())));
+  CHECK (!is_atomic (tree (1, tree (), tree ())));
+  CHECK (!is_atomic (tree (2, tree (), tree ())));
+}
 
 TEST_CASE ("test_is_compound") {
-  CHECK (is_compound (tree (/*FRAC*/ 70, 3)));
+  CHECK (is_compound (tree (70, 3)));
+  CHECK (is_compound (tree (1, tree ())));
   CHECK (!is_compound (tree ()));
+  CHECK (!is_compound (tree (0, tree ())));
 }
 
 TEST_CASE ("test_is_bool") {
@@ -55,6 +62,9 @@ TEST_CASE ("test_is_generic") {
   CHECK (!is_generic (tree (11, 1234)));
   CHECK (!is_generic (tree (11, 4)));
   CHECK (!is_generic (tree (12, 2)));
+  CHECK (is_generic (tree (-1, 1)));
+  CHECK (is_generic (tree (-2, 1)));
+  CHECK (is_generic (tree (-3, 1)));
 }
 
 TEST_CASE ("test_arity") {
