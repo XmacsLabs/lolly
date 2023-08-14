c34f3c9a7ef6221370bcfc450621d230d9a0fec9
charonxin <1656746727@qq.com>
2023-08-14 17:23:21 +0800

    format

--- tests/Kernel/Types/generic_tree_test.cpp
+++ tests/Kernel/Types/generic_tree_test.cpp
@@ -5,8 +5,7 @@
 #include "tree.hpp"
 
 TEST_CASE ("test as") {
-  CHECK_EQ (as<double,int> (1), 1.0);
-  CHECK_EQ (as<int,double> (1.0), 1);
-  CHECK_EQ (as<int,double> (1.5), 1);
+  CHECK_EQ (as<double, int> (1), 1.0);
+  CHECK_EQ (as<int, double> (1.0), 1);
+  CHECK_EQ (as<int, double> (1.5), 1);
 }
-
