fbc7a942af967a9ebf5a2a1e3fe5a8c8026a586a
Ye Tianxin <1656746727@qq.com>
2023-08-18 17:32:44 +0800

    [1_2] doc: modify basic.hpp and add doc for blackbox.hpp (#66)

--- Kernel/Abstractions/basic.hpp
+++ Kernel/Abstractions/basic.hpp
@@ -1,13 +1,10 @@
 
-/******************************************************************************
- * MODULE     : basic.hpp
- * DESCRIPTION: see basic.cpp
- * COPYRIGHT  : (C) 1999  Joris van der Hoeven
- *******************************************************************************
- * This software falls under the GNU general public license version 3 or later.
- * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
- * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
- ******************************************************************************/
+/** \file basic.hpp
+ *  \copyright GPLv3
+ *  \details defines basic types and macros used throughout the kernel.
+ *  \author Joris van der Hoeven
+ *  \date   1999
+ */
 
 #ifndef BASIC_H
 #define BASIC_H
