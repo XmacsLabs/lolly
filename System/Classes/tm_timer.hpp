06ae4234442262ed7b74d755264abe99593a789c
沈浪熊猫儿 <shenda@ustc.edu>
2023-08-21 22:35:49 +0800

    fix

--- System/Classes/tm_timer.hpp
+++ System/Classes/tm_timer.hpp
@@ -17,7 +17,7 @@
 #ifndef __FreeBSD__
 #ifndef HAVE_TIME_T
 #define HAVE_TIME_T
-#if (defined OS_SUN || defined OS_LINUX || defined OS_MACOSX)
+#if (defined OS_SUN || defined OS_LINUX || defined OS_MACOS)
 typedef long time_t;
 #endif
 #endif
