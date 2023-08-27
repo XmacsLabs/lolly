b9e33bbeb9d282031a3a316224596bf3676ba15d
沈浪熊猫儿 <shenda@ustc.edu>
2023-08-27 21:26:32 +0800

    [10_7] Migrate sys_utils

---
+++ Plugins/Windows/win_sys_utils.cpp
@@ -0,0 +1,44 @@
+
+/******************************************************************************
+ * MODULE     : mingw_sys_utils.cpp
+ * DESCRIPTION: external command handling
+ * COPYRIGHT  : (C) 2015  Gregoire LECERF, Denis RAUX
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
+#include "win_sys_utils.hpp"
+#include "analyze.hpp"
+#include "tm_timer.hpp"
+#include "win_utf8_compat.hpp"
+
+namespace lolly {
+
+#ifndef SECURITY_WIN32
+#define SECURITY_WIN32
+#endif
+
+#include <windows.h>
+#include <windef.h>
+#include <basetsd.h>
+#include <ntsecapi.h>
+#include <security.h>
+#include <wtypesbase.h>
+
+string
+win_get_username () {
+  const int MAX_LEN= 100;
+  WCHAR     buffer[MAX_LEN];
+  DWORD     len;
+
+  // This API must be called twice, otherwise it returns an empty use name
+  GetUserNameExW (NameDisplay, buffer, &len);
+  GetUserNameExW (NameDisplay, buffer, &len);
+
+  if (len == 0) return string ("");
+  else return string (nowide::narrow (buffer).c_str ());
+}
+
+} // namespace lolly
