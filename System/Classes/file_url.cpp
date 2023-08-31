ea43023a1eb5156e83b399163e43d886bc708d7c
沈浪熊猫儿 <shenda@ustc.edu>
2023-08-31 21:57:11 +0800

    license header

--- System/Classes/file_url.cpp
+++ System/Classes/file_url.cpp
@@ -1,3 +1,14 @@
+
+/******************************************************************************
+ * MODULE     : file_url.cpp
+ * DESCRIPTION: url rooted with file://
+ * COPYRIGHT  : (C) 2023  Darcy Shen
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
 #include "url.hpp"
 
 file_url::file_url (string name)
