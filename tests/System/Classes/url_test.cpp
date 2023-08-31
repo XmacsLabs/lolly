216493dd6ee72fb037d023132e883a5d8fc4cec3
Darcy Shen <sadhen@zoho.com>
2023-08-31 21:16:36 +0800

    [10_8] Test as_string of url

--- tests/System/Classes/url_test.cpp
+++ tests/System/Classes/url_test.cpp
@@ -9,6 +9,49 @@
 #include "doctest/doctest.h"
 #include "url.hpp"
 
+url ustc_edu   = url_system ("https://ustc.edu.cn");
+url texmacs_org= url_system ("https://texmacs.org");
+url none_url   = url_none ();
+url file_root  = url_root ("file");
+url wsl_ubuntu = url_system ("\\\\wsl.localhost\\Ubuntu");
+
+#if defined(OS_MINGW) || defined(OS_WIN)
+TEST_CASE ("as_string on windows") {
+  url win_c_windows= url_system ("C:/Windows");
+  url win_c        = url_system ("C:/");
+  url system_root  = url_system ("%SystemRoot%");
+  url windir       = url_system ("%windir%");
+#ifdef OS_WIN
+  CHECK_EQ (as_string (win_c) == string ("C:{/}"), true);
+#else
+  CHECK_EQ (as_string (win_c) == string ("C:\\"), true);
+#endif
+#ifdef OS_WIN
+  CHECK_EQ (as_string (win_c_windows) == string ("C:/Windows"), true);
+#else
+  CHECK_EQ (as_string (win_c_windows) == string ("C:\\Windows"), true);
+#endif
+  CHECK_EQ (as_string (system_root) == string ("%SystemRoot%"), true);
+  CHECK_EQ (as_string (windir) == string ("%windir%"), true);
+}
+#endif
+
+TEST_CASE ("is_none") {
+  CHECK (!is_none (ustc_edu));
+  CHECK (!is_none (texmacs_org));
+  CHECK (!is_none (wsl_ubuntu));
+  CHECK (!is_none (file_root));
+  CHECK (is_none (none_url));
+}
+
+TEST_CASE ("is_root") {
+  CHECK (!is_root (ustc_edu));
+  CHECK (!is_root (texmacs_org));
+  CHECK (!is_root (wsl_ubuntu));
+  CHECK (is_root (file_root));
+  CHECK (!is_root (none_url));
+}
+
 TEST_CASE ("suffix") {
   // empty suffix should work
   url no_suffix= url ("/a/b/c/d/no_suffix");
