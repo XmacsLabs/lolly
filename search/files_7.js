6348d5deef746c6028ceb70cb435d9dac370aea3
darcy-shen <sadhen@zoho.com>
2023-09-06 12:21:55 +0000

    Build: (8939788) [12_9] improve doctests with/without tbox
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- search/files_7.js
+++ search/files_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['list_2ehpp_0',['list.hpp',['../list_8hpp.html',1,'']]],
   ['list_2eipp_1',['list.ipp',['../list_8ipp.html',1,'']]],
-  ['list_5ftest_2ecpp_2',['list_test.cpp',['../list__test_8cpp.html',1,'']]]
+  ['list_5ftest_2ecpp_2',['list_test.cpp',['../list__test_8cpp.html',1,'']]],
+  ['lolly_5fdoctests_2ehpp_3',['lolly_doctests.hpp',['../lolly__doctests_8hpp.html',1,'']]]
 ];
