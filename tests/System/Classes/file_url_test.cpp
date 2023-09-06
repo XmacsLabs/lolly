d1da7c9e1324183a40c46f5e7579345b8c08e4a5
沈浪熊猫儿 <shenda@ustc.edu>
2023-09-06 19:23:26 +0800

    fix

--- tests/System/Classes/file_url_test.cpp
+++ tests/System/Classes/file_url_test.cpp
@@ -14,10 +14,10 @@ url system_root  = url_system ("%SystemRoot%");
 url windir       = url_system ("%windir%");
 
 TEST_CASE ("as_string on windows") {
-  CHECK_EQ (as_string (win_c) == string ("C:\\"), true);
-  CHECK_EQ (as_string (win_c_windows) == string ("C:\\Windows"), true);
-  CHECK_EQ (as_string (system_root) == string ("%SystemRoot%"), true);
-  CHECK_EQ (as_string (windir) == string ("%windir%"), true);
+  string_eq (as_string (win_c), "C:\\");
+  string_eq (as_string (win_c_windows), "C:\\Windows");
+  string_eq (as_string (system_root), "%SystemRoot%");
+  string_eq (as_string (windir), "%windir%");
 }
 #else
 url unix_root= url_system ("/");
@@ -25,6 +25,6 @@ url unix_tmp = url_system ("/tmp");
 
 TEST_CASE ("as_string on nix") {
   string_eq (as_string (unix_root), "/");
-  string_eq (as_string (unix_tmp) "/tmp", true);
+  string_eq (as_string (unix_tmp), "/tmp");
 }
 #endif
