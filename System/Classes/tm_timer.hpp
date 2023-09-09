c7709951cb8ba05eb1832a46ee3395dabd4e6a5b
Paradisum <2513209473@qq.com>
2023-09-09 14:54:35 +0800

    remove complex time_t

--- System/Classes/tm_timer.hpp
+++ System/Classes/tm_timer.hpp
@@ -12,19 +12,19 @@
 #ifndef TIMER_H
 #define TIMER_H
 
-#ifndef OS_WIN
+// #ifndef OS_WIN
 
-#ifndef __FreeBSD__
-#ifndef HAVE_TIME_T
-#define HAVE_TIME_T
-#if (defined OS_LINUX || defined OS_MACOS)
-typedef long time_t;
-#endif
-#endif
-#endif
-#else
+// #ifndef __FreeBSD__
+// #ifndef HAVE_TIME_T
+// #define HAVE_TIME_T
+// #if (defined OS_LINUX || defined OS_MACOS)
+// typedef long time_t;
+// #endif
+// #endif
+// #endif
+// #else
 #include <time.h>
-#endif
+// #endif
 
 #ifdef HAVE_GETTIMEOFDAY
 #include <sys/time.h>
