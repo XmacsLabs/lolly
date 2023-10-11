77c49f0c160b35eb58b14fd3a12c765622a03f4f
jingkaimori <jingkaimori@gmail.com>
2023-10-11 12:22:42 +0800

    [12_21] refactor implementation and interface of file

--- System/Files/file.hpp
+++ System/Files/file.hpp
@@ -45,8 +45,8 @@ void copy (url u1, url u2);
 bool   load_string (url file_name, string& s, bool fatal);
 string string_load (url u);
 bool   save_string (url file_name, const string& s, bool fatal= false);
-void   string_save (string s, url u);
-bool   append_string (url u, string s, bool fatal);
-void   string_append_to_file (string s, url u);
+void   string_save (const string& s, url u);
+bool   append_string (url u, const string& s, bool fatal);
+void   string_append_to_file (const string& s, url u);
 void   append_to (url what, url to);
 #endif
