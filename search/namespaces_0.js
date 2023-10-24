e93f5f3346917b267a9546a333cd0026f1fd8d1d
darcy-shen <sadhen@zoho.com>
2023-10-24 07:40:22 +0000

    Build: (1b1a71e) [12_20] Rename system to call and check_output

--- search/namespaces_0.js
+++ search/namespaces_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['data_0',['data',['../namespacelolly_1_1data.html',1,'lolly']]],
   ['io_1',['io',['../namespacelolly_1_1io.html',1,'lolly']]],
-  ['lolly_2',['lolly',['../namespacelolly.html',1,'']]]
+  ['lolly_2',['lolly',['../namespacelolly.html',1,'']]],
+  ['system_3',['system',['../namespacelolly_1_1system.html',1,'lolly']]]
 ];
