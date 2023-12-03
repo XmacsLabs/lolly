4ad4869d1e3bd86c58fe1578ece19598cf392331
Darcy Shen <da@liii.pro>
2023-12-03 22:54:29 +0800

    [2_1] Revert: Improve << of hashset<string>
    This reverts commit 3c0124cda791277777e9f65123eaac48803f3576.

--- tests/Kernel/Containers/hashset_test.cpp
+++ tests/Kernel/Containers/hashset_test.cpp
@@ -11,8 +11,7 @@ TEST_CASE ("test contains") {
 
 TEST_CASE ("test init") {
   auto set= hashset<string> ();
-  set << "hello"
-      << "world";
+  set << string ("hello") << string ("world");
   CHECK_EQ (set->contains ("hello"), true);
   CHECK_EQ (set->contains ("world"), true);
   CHECK_EQ (N (set), 2);
@@ -22,12 +21,9 @@ TEST_CASE ("test remove") {
   auto set1= hashset<string> ();
   auto set2= hashset<string> ();
   CHECK_EQ (N (set1), N (set2));
-  set1 << "aaa"
-       << "bbb";
+  set1 << string ("aaa") << string ("bbb");
   CHECK_EQ (N (set1), N (set2) + 2);
-  set2 << "aaa"
-       << "bbb"
-       << "ccc";
+  set2 << string ("aaa") << string ("bbb") << string ("ccc");
   set2->remove (string ("bbb"));
   CHECK_EQ (N (set1), N (set2));
 }
@@ -35,29 +31,23 @@ TEST_CASE ("test remove") {
 TEST_CASE ("test operator <=") {
   auto out= tm_ostream ();
   auto set= hashset<string> ();
-  set << "aaa"
-      << "bbb";
+  set << string ("aaa") << string ("bbb");
   auto set1= hashset<string> ();
-  set1 << "aaa"
-       << "bbb"
-       << "ccc";
+  set1 << string ("aaa") << string ("bbb") << string ("ccc");
   auto out1= tm_ostream ();
   CHECK_EQ (set <= set1, true);
 }
 
 TEST_CASE ("test copy") {
   auto set1= hashset<string> (), set2= hashset<string> ();
-  set1 << "a"
-       << "b"
-       << "c";
+  set1 << string ("a") << string ("b") << string ("c");
   set2= copy (set1);
   CHECK_EQ (set2->contains (string ("a")), true);
   CHECK_EQ (set2->contains (string ("b")), true);
   CHECK_EQ (set2->contains (string ("c")), true);
   CHECK_EQ (N (set1) == N (set2), true);
   // Test utf-8 for Chinese
-  set1 << "你好"
-       << "世界";
+  set1 << string ("你好") << string ("世界");
   set2= copy (set1);
   CHECK_EQ (set2->contains (string ("你好")), true);
   CHECK_EQ (set2->contains (string ("世界")), true);
