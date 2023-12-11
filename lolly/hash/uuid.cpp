a0f5fba009669a710cfbe3249e8a287940786461
Darcy Shen <da@liii.pro>
2023-12-11 13:46:20 +0800

    [20_1] uuid in namespace lolly::hash

---
+++ lolly/hash/uuid.cpp
@@ -0,0 +1,27 @@
+
+/******************************************************************************
+ * MODULE     : uuid.cpp
+ * DESCRIPTION: UUID generation
+ * COPYRIGHT  : (C) 2023  Darcy Shen
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
+#include "uuid.hpp"
+#include "tbox/tbox.h"
+
+namespace lolly {
+namespace hash {
+string
+uuid_make () {
+  tb_char_t        uuid[37];
+  const tb_char_t* ret= tb_uuid4_make_cstr (uuid, tb_null);
+  if (ret == NULL) {
+    TM_FAILED ("Failed to generate UUID");
+  }
+  return string (ret);
+}
+} // namespace hash
+} // namespace lolly
