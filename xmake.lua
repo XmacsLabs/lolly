b34f44bb3a505e2e9d2897bd3cb831aaa722238d
Darcy Shen <da@liii.pro>
2024-01-03 19:53:14 +0800

    [1.3] bump to 1.3.13

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ set_allowedmodes("releasedbg", "release", "debug")
 add_rules("mode.debug")
 
 set_project("lolly")
-LOLLY_VERSION= "1.3.12"
+LOLLY_VERSION= "1.3.13"
 
 set_languages("c++17")
 includes("@builtin/check")
