d5f16f91ce2bd3b0f4db7716d16a1a55d2925089
Yiheng Wu <36156959+jingkaimori@users.noreply.github.com>
2024-02-15 09:19:15 +0800

    [8_8] Optimize comparison between string

--- Kernel/Types/string.cpp
+++ Kernel/Types/string.cpp
@@ -181,24 +181,22 @@ operator* (string a, const char* b) {
 
 bool
 operator< (string s1, string s2) {
-  int i;
-  for (i= 0; i < N (s1); i++) {
-    if (i >= N (s2)) return false;
+  int i, n1= N (s1), n2= N (s2), nmin= min (n1, n2);
+  for (i= 0; i < nmin; i++) {
     if (s1[i] < s2[i]) return true;
     if (s2[i] < s1[i]) return false;
   }
-  return i < N (s2);
+  return n1 < n2;
 }
 
 bool
 operator<= (string s1, string s2) {
-  int i;
-  for (i= 0; i < N (s1); i++) {
-    if (i >= N (s2)) return false;
+  int i, n1= N (s1), n2= N (s2), nmin= min (n1, n2);
+  for (i= 0; i < nmin; i++) {
     if (s1[i] < s2[i]) return true;
     if (s2[i] < s1[i]) return false;
   }
-  return true;
+  return n1 <= n2;
 }
 
 tm_ostream&
