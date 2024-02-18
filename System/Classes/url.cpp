7b64b8ffedec5282fbd554bf0cc6bc9d296fa481
Darcy Shen <da@liii.pro>
2024-02-18 17:02:41 +0800

    Revert "wip"
    This reverts commit c903115e14f1dc2828e7417fc428d3d6d4081e33.

--- System/Classes/url.cpp
+++ System/Classes/url.cpp
@@ -212,7 +212,7 @@ url_get_name (string s, int type, int i) {
   url ret= u_list->item;
   u_list = u_list->next;
   while (!is_nil (u_list)) {
-    ret   = as_url (url_tuple ("concat", u_list->item->t, ret->t));
+    ret   = u_list->item * ret;
     u_list= u_list->next;
   }
   return ret;
