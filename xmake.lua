31145e70445dae372a1ce2b76af082cb8f6f9c8e
Darcy Shen <da@liii.pro>
2024-03-09 19:37:38 +0800

    [1.4] bump to 1.4.2

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ set_allowedmodes("releasedbg", "release", "debug")
 add_rules("mode.debug")
 
 set_project("lolly")
-LOLLY_VERSION= "1.4.1"
+LOLLY_VERSION= "1.4.2"
 
 set_languages("c++17")
 includes("@builtin/check")
