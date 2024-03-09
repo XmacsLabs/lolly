f0748a3dfc2b8b921025fbe7ac33d6f68f27b34f
darcy-shen <sadhen@zoho.com>
2024-03-09 13:21:16 +0000

    Build: (8e271b5) [1.4] bump to 1.4.4

--- search/all_2d.js
+++ search/all_2d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['方法一：安装到本地_0',['方法一：安装到本地',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md57',1,'']]],
-  ['方法二：添加xmake依赖_1',['方法二：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md58',1,'']]],
-  ['方法1：本地安装_2',['方法1：本地安装',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md67',1,'']]],
-  ['方法2：添加xmake依赖_3',['方法2：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md68',1,'']]]
+  ['方法一：安装到本地_0',['方法一：安装到本地',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md50',1,'']]],
+  ['方法二：添加xmake依赖_1',['方法二：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md52',1,'']]],
+  ['方法1：本地安装_2',['方法1：本地安装',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md58',1,'']]],
+  ['方法2：添加xmake依赖_3',['方法2：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md59',1,'']]]
 ];
