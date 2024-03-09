cc0ea0d501bbc5233859f89c13ac18e673574d02
Darcy Shen <da@liii.pro>
2024-03-09 20:31:27 +0800

    [9_3] lolly_tree: copy

--- lolly/data/lolly_tree.hpp
+++ lolly/data/lolly_tree.hpp
@@ -411,5 +411,18 @@ operator<< (tm_ostream& out, lolly_tree<T> t) {
   return out;
 }
 
+template <typename T>
+inline lolly_tree<T>
+copy (lolly_tree<T> t) {
+  if (is_atomic (t)) return tree (copy (t->label));
+  else {
+    int           i, n= N (t);
+    lolly_tree<T> t2 (t, n);
+    for (i= 0; i < n; i++)
+      t2[i]= copy (t[i]);
+    return t2;
+  }
+}
+
 } // namespace data
 } // namespace lolly
