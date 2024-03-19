2412fd0e236806b3ebc9941f8066d85762ff615d
jingkaimori <jingkaimori@gmail.com>
2024-03-19 18:22:36 +0800

    add conversion between lolly_string_view and std::basic_string

--- lolly/io/http.cpp
+++ lolly/io/http.cpp
@@ -77,56 +77,54 @@ response_to_tree (cpr::Response r, string url) {
   return ret;
 }
 
+using string_view= lolly::data::lolly_string_view<char>;
+
 static cpr::Header
 as_cpr_header (http_headers hmap) {
   cpr::Header      header= cpr::Header{};
   iterator<string> it    = iterate (hmap);
   while (it->busy ()) {
-    string key                          = it->next ();
-    header[std::string (c_string (key))]= c_string (hmap[key]);
+    string key               = it->next ();
+    header[(string_view) key]= (string_view) hmap[key];
   }
   return header;
 }
 
 http_tree
 http_get (url u, http_headers headers) {
-  string       u_str = as_string (u);
-  c_string     u_cstr= c_string (u_str);
+  string       u_str= as_string (u);
   cpr::Session session;
-  session.SetUrl (cpr::Url (u_cstr));
+  session.SetUrl (cpr::Url ((string_view) u_str));
   session.SetHeader (as_cpr_header (headers));
   session.SetUserAgent (
-      cpr::UserAgent (std::string (c_string (headers[HTTP_USER_AGENT]))));
+      cpr::UserAgent ((string_view) headers[HTTP_USER_AGENT]));
   cpr::Response r= session.Get ();
   return response_to_tree (r, u_str);
 }
 
 http_tree
 http_head (url u, http_headers headers) {
-  string       u_str = as_string (u);
-  c_string     u_cstr= c_string (u_str);
+  string       u_str= as_string (u);
   cpr::Session session;
-  session.SetUrl (cpr::Url (u_cstr));
+  session.SetUrl (cpr::Url ((string_view) u_str));
   session.SetHeader (as_cpr_header (headers));
   session.SetUserAgent (
-      cpr::UserAgent (std::string (c_string (headers[HTTP_USER_AGENT]))));
+      cpr::UserAgent ((string_view) headers[HTTP_USER_AGENT]));
   cpr::Response r= session.Head ();
   return response_to_tree (r, u_str);
 }
 
 http_tree
 download (url from, url to, http_headers headers) {
-  string   from_str = as_string (from);
-  c_string from_cstr= c_string (from_str);
-  string   to_str   = as_string (to);
-  c_string to_cstr  = c_string (to_str);
+  string from_str= as_string (from);
+  string to_str  = as_string (to);
 
   cpr::Session session;
-  session.SetUrl (cpr::Url (from_cstr));
+  session.SetUrl (cpr::Url ((string_view) from_str));
   session.SetHeader (as_cpr_header (headers));
   session.SetUserAgent (
-      cpr::UserAgent (std::string (c_string (headers[HTTP_USER_AGENT]))));
-  std::ofstream to_stream (to_cstr, std::ios::binary);
+      cpr::UserAgent ((string_view) headers[HTTP_USER_AGENT]));
+  std::ofstream to_stream ((string_view) to_str, std::ios::binary);
   cpr::Response r= session.Download (to_stream);
   return response_to_tree (r, from_str);
 }
