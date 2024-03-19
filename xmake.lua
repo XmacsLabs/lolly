be0cb6a3955334df141920e680ef1932fca1f152
jingkaimori <jingkaimori@gmail.com>
2024-03-19 23:32:39 +0800

    [1.4] bump to 1.4.16
    * fix name lookup in lolly_tree
    * fix inconsistent behaviour between as_int and is_int

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ set_allowedmodes("releasedbg", "release", "debug")
 add_rules("mode.debug")
 
 set_project("lolly")
-LOLLY_VERSION= "1.4.15"
+LOLLY_VERSION= "1.4.16"
 
 set_languages("c++17")
 includes("@builtin/check")
