f58ea44007f62e03a50cc43fbdc28578b81a1ba6
jingkaimori <jingkaimori@users.noreply.github.com>
2024-04-14 06:11:44 +0000

    Build: (f622a7d) Merge 11a45be49b4af1fc6cc90dae71ea41fba0ebdcc1 into a8469822f28000bfd75407e0c9454828d344ced3

--- search/all_2d.js
+++ search/all_2d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['方法一：安装到本地_0',['方法一：安装到本地',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md61',1,'']]],
-  ['方法二：添加xmake依赖_1',['方法二：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md62',1,'']]],
+  ['方法一：安装到本地_0',['方法一：安装到本地',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md60',1,'']]],
+  ['方法二：添加xmake依赖_1',['方法二：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md61',1,'']]],
   ['方法1：本地安装_2',['方法1：本地安装',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md68',1,'']]],
   ['方法2：添加xmake依赖_3',['方法2：添加xmake依赖',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md69',1,'']]]
 ];
