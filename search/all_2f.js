d64d1b72aceba591373ba9ac1e7f160a982ab847
jingkaimori <jingkaimori@users.noreply.github.com>
2024-04-14 05:37:24 +0000

    Build: (a1998d1) Merge 8654f8e63c68b445287ea23678dda3d790b31b8a into a8469822f28000bfd75407e0c9454828d344ced3

--- search/all_2f.js
+++ search/all_2f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['第一步：安装xmake_0',['第一步：安装xmake',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md59',1,'']]],
-  ['第三步：编译测试_1',['第三步：编译测试',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md63',1,'']]],
-  ['第二步：安装与使用_2',['第二步：安装与使用',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md60',1,'']]]
+  ['第一步：安装xmake_0',['第一步：安装xmake',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md52',1,'']]],
+  ['第三步：编译测试_1',['第三步：编译测试',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md56',1,'']]],
+  ['第二步：安装与使用_2',['第二步：安装与使用',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md53',1,'']]]
 ];
