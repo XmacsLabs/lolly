598b046f216c774be74d873825a97b3989bcf1a5
jingkaimori <jingkaimori@users.noreply.github.com>
2024-04-15 07:14:27 +0000

    Build: (32df88a) Merge 3c1961fd85956920019625006ad8908779734bba into f7827105fba8ca66949cf7c961d0a37acc88aa00

--- search/all_26.js
+++ search/all_26.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['使用xmake_0',['使用xmake',['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md52',1,'使用xmake'],['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md57',1,'使用xmake']]]
+  ['使用xmake_0',['使用xmake',['../md_docs_2zh_2guide_2Develop__on__Windows.html#autotoc_md61',1,'使用xmake'],['../md_docs_2zh_2guide_2Develop__on__Linux.html#autotoc_md59',1,'使用xmake']]]
 ];
